a2e1de73e86b74af4a8ba431f2aaf072f1b02125
RehdBlob <rehdblob@gmail.com>
2012-08-17 21:49:40 -0700

    Start integration of previous classes with JavaFX

--- src/smp/fx/SuperMarioPaintFX.java
+++ src/smp/fx/SuperMarioPaintFX.java
@@ -1,14 +1,8 @@
 package smp.fx;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
