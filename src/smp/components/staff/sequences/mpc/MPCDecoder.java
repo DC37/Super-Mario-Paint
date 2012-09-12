34156d5f6b083f617fbbbc925437f3699ff43fcc
unknown <rehdblob@gmail.com>
2012-09-12 15:58:56 -0700

    More on the MPC Decoder

--- src/smp/components/staff/sequences/mpc/MPCDecoder.java
+++ src/smp/components/staff/sequences/mpc/MPCDecoder.java
@@ -24,58 +24,17 @@ public class MPCDecoder {
     private static final int NUM_TRACKS = 19;
 
     /**
-     * Links an <code>InstrumentIndex</code> to a Character.
-     * @author RehdBlob
-     * @since 2012.09.11
-     */
-    private enum MPCInstrumentIndex {
-        a (InstrumentIndex.MARIO),
-        b (InstrumentIndex.MUSHROOM),
-        c (InstrumentIndex.YOSHI),
-        d (InstrumentIndex.STAR),
-        e (InstrumentIndex.FLOWER),
-        f (InstrumentIndex.GAMEBOY),
-        g (InstrumentIndex.DOG),
-        h (InstrumentIndex.CAT),
-        i (InstrumentIndex.PIG),
-        j (InstrumentIndex.SWAN),
-        k (InstrumentIndex.FACE),
-        l (InstrumentIndex.PLANE),
-        m (InstrumentIndex.BOAT),
-        n (InstrumentIndex.CAR),
-        o (InstrumentIndex.HEART),
-        p (InstrumentIndex.PIRANHA),
-        q (InstrumentIndex.SHYGUY),
-        r (InstrumentIndex.COIN),
-        s (InstrumentIndex.BOO);
-
-        private InstrumentIndex ind;
-
-        private MPCInstrumentIndex(InstrumentIndex i) {
-            ind = i;
-        }
-
-        public InstrumentIndex getInstrument() {
-            return ind;
-        }
-
-        public static InstrumentIndex valueOf(char c) {
-            String str = "" + c;
-            return MPCInstrumentIndex.valueOf(str).getInstrument();
-        }
-
-
-    }
-
-    /**
      * Decodes a Mario Paint Composer song into an SMP-readable format.
      * Uses <code>TextUtil</code> from <code>MPCTxtTools</code>.
      * @param in The input String that contains (supposedly) Mario Paint
      * Composer song file data.
      * @throws ParseException If someone tries to feed this method an invalid
      * text file.
+     * @throws InvalidMidiDataException If the sequence population fails to
+     * work correctly.
      */
-    public static SMPSequence decode(String in) throws ParseException {
+    public static SMPSequence decode(String in)
+            throws ParseException, InvalidMidiDataException {
         if (in.indexOf('*') == -1 || in.isEmpty() || in == null) {
             throw new ParseException("Invalid MPC Text File.", 0);
         }
@@ -101,7 +60,19 @@ public class MPCDecoder {
             ArrayList<String> songData, String tempo)
                     throws InvalidMidiDataException {
         SMPSequence song = new SMPSequence(NUM_TRACKS);
+        song.setTime(timeSig);
+        song.setData(decodeSong(songData));
+        song.setTempo(tempo);
+        return song;
+    }
 
+    /**
+     * 
+     * @param songData
+     * @return The MIDI events in the MPC sequence
+     */
+    private static SongData decodeSong(ArrayList<String> songData) {
+        // TODO Auto-generated method stub
         return null;
     }
 }
