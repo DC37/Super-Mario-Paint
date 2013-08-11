e39b338912e239e0713ccb0583748c356e615a85
DC37 <rehdblob@gmail.com>
2013-08-11 00:28:01 -0700

    The actual quick fix: Debug levels

--- src/smp/SuperMarioPaint.java
+++ src/smp/SuperMarioPaint.java
@@ -183,7 +183,15 @@ public class SuperMarioPaint extends Application {
             launch(args);
         } else if (args.length > 0 && (args[0].equals("--debug")
                 || args[0].equals("--d"))) {
-            Settings.debug = 1;
+            if (args[1] != null) {
+                try {
+                    Settings.setDebug(Integer.parseInt(args[1]));
+                } catch (NumberFormatException e) {
+                    Settings.setDebug(1);
+                }
+            } else {
+                Settings.setDebug(1);
+            }
             launch(args);
         }
 
