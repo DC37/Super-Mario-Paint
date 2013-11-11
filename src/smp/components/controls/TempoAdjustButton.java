3499c51351a53e03ee01e28f30ba09f98024cc36
DC37 <rehdblob@gmail.com>
2013-11-10 20:01:43 -0800

    Fixed tempo increments.

--- src/smp/components/controls/TempoAdjustButton.java
+++ src/smp/components/controls/TempoAdjustButton.java
@@ -1,7 +1,11 @@
 package smp.components.controls;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javafx.application.Platform;
 import javafx.beans.property.StringProperty;
+import javafx.event.EventHandler;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import smp.components.general.ImagePushButton;
@@ -21,6 +25,9 @@ public class TempoAdjustButton extends ImagePushButton {
     /** Tells us whether this is a plus or minus button. */
     private boolean isPositive;
 
+    /** This is a timer object for click-and-hold. */
+    private Timer t;
+
     /**
      * Default constructor
      * @param i The <code>ImageView</code> object that we want this
@@ -28,7 +35,37 @@ public class TempoAdjustButton extends ImagePushButton {
      */
     public TempoAdjustButton(ImageView i) {
         super(i);
+        t = new Timer();
+        overrideHoldAndRelease();
 
+    }
+
+    /**
+     * Gives us some click-and-hold functionality of these tempo adjust
+     * buttons.
+     */
+    private void overrideHoldAndRelease() {
+        theImage.setOnMousePressed(
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        TimerTask tt = new clickHold();
+                        reactPressed(event);
+                        t.scheduleAtFixedRate(tt, 100, 10);
+                        event.consume();
+                    }
+                });
+
+        theImage.setOnMouseReleased(
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        reactReleased(event);
+                        t.cancel();
+                        t = new Timer();
+                        event.consume();
+                    }
+                });
     }
 
     /**
@@ -88,5 +125,18 @@ public class TempoAdjustButton extends ImagePushButton {
         });
     }
 
+    /**
+     * This is a timer task that increments the tempo of the song.
+     * @author RehdBlob
+     * @since 2013.11.10
+     */
+    class clickHold extends TimerTask {
+
+        @Override
+        public void run() {
+            addTempo(1);
+        }
+
+    }
 
 }
