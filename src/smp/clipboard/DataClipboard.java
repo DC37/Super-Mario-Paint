0fc3a313e26622f570bea79dc36a97333113ed23
j574y923 <j574y923@gmail.com>
2017-10-11 22:23:27 -0500

    add first copy paste code

--- src/smp/clipboard/DataClipboard.java
+++ src/smp/clipboard/DataClipboard.java
@@ -1,10 +1,53 @@
 package smp.clipboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import smp.ImageLoader;
+import smp.components.InstrumentIndex;
+import smp.components.Values;
 import smp.components.staff.Staff;
+import smp.components.staff.sequences.StaffNote;
+import smp.components.staff.sequences.StaffNoteLine;
 import smp.fx.SMPFXController;
+
+/**
+ * This class can be used to keep track of the bounds that we will copy, delete,
+ * etc. notes from.
+ *
+ */
+class SelectionBounds {
+	private int lineBegin;
+	private int lineEnd;
+	private int positionBegin;
+	private int positionEnd;
+
+	public SelectionBounds(int lb, int le, int pb, int pe) {
+		this.lineBegin = lb;
+		this.lineEnd = le;
+		this.positionBegin = pb;
+		this.positionEnd = pe;
+	}
+
+	public int getLineBegin() {
+		return lineBegin;
+	}
+
+	public int getLineEnd() {
+		return lineEnd;
+	}
+
+	public int getPositionBegin() {
+		return positionBegin;
+	}
+
+	public int getPositionEnd() {
+		return positionEnd;
+	}
+}
 
 public class DataClipboard {
 
@@ -13,15 +56,128 @@ public class DataClipboard {
 	private ImageLoader il;
 	private StackPane rubberBandLayer;
 	private RubberBandEventHandler rbeh;
+
+	private DataClipboardFunctions functions;
 	
+	/* A list that keeps track of all the selections' bounds made by the user */
+	private ArrayList<SelectionBounds> selection;
+	
+	/* The list that will keep track of copied notes */
+	private ArrayList<StaffNoteLine> data;
+	
+	/* Corresponds with the first selection line */
+	private int dataLineBegin = 0;
+	/* Corresponds with the last selection line */
+	private int dataLineEnd = -1;
+
 	public DataClipboard(Staff st, SMPFXController ct, ImageLoader im) {
 
-        il = im;
-        theStaff = st;
+		il = im;
+		theStaff = st;
 		controller = ct;
-		rubberBandLayer = controller.getRubberBandLayer();
-		rbeh = new RubberBandEventHandler(st, rubberBandLayer);
-		rubberBandLayer.addEventHandler(MouseEvent.ANY, rbeh);
+
+		selection = new ArrayList<>();
+		data = new ArrayList<>(Values.DEFAULT_LINES_PER_SONG);
+		for (int i = 0; i < Values.DEFAULT_LINES_PER_SONG; i++)
+			data.add(new StaffNoteLine());
+		
+		//temp? merge the two classes together?
+		functions = new DataClipboardFunctions(this, theStaff, im);
 	}
 
+	/**
+	 * Add a new SelectionBounds to the selection ArrayList. 
+	 * 
+	 * This will be used to keep track of all the selections made by the user.
+	 * 
+	 * @param lb lineBegin
+	 * @param le lineEnd
+	 * @param pb positionBegin
+	 * @param pe positionEnd
+	 */
+	public void addSelection(int lb, int le, int pb, int pe) {
+		selection.add(new SelectionBounds(lb, le, pb, pe));
+	}
+
+	/**
+	 * Clear the selection ArrayList.
+	 */
+	public void clearSelection() {
+		selection.clear();
+	}
+	
+	public ArrayList<SelectionBounds> getSelection() {
+		return selection;
+	}
+	
+	/**
+	 * Copy information from note. Add new note into data.
+	 * 
+	 * @param line
+	 *            where the note occurs
+	 * @param note
+	 *            that will have its info copied
+	 */
+	public void copyNote(int line, StaffNote note) {
+		StaffNote newNote = new StaffNote(note.getInstrument(), note.getPosition(), note.getAccidental());
+		data.get(line).add(newNote);
+	}
+	
+	public void clearData() {
+		for (int i = dataLineBegin; i <= dataLineEnd; i++) {
+			StaffNoteLine line = data.get(i);
+			line.getNotes().clear();
+		}
+		dataLineBegin = 0;
+		dataLineEnd = -1;
+	}
+	
+	public ArrayList<StaffNoteLine> getData() {
+		return data;
+	}
+	
+	public int getDataLineBegin() {
+		return dataLineBegin;
+	}
+	
+	public int getDataLineEnd() {
+		return dataLineEnd;
+	}
+	
+	public void setDataLineBegin(int line) {
+		dataLineBegin = line;
+	}
+	
+	public void setDataLineEnd(int line) {
+		dataLineEnd = line;
+	}
+
+	/**
+	 * convenience method, update dataLineBegin only if line is less. Should
+	 * call this because dataLineBegin will be used to define bounds of data.
+	 * 
+	 * @param line
+	 *            to update dataLineBegin to
+	 */
+	public void updateDataLineBegin(int line) {
+		if (line < dataLineBegin)
+			dataLineBegin = line;
+	}
+
+	/**
+	 * convenience method, update dataLineEnd only if line is more. Should call
+	 * this because dataLineEnd will be used to define bounds of data.
+	 * 
+	 * @param line
+	 *            to update dataLineEnd to
+	 */
+	public void updateDataLineEnd(int line) {
+		if(line > dataLineEnd)
+			dataLineEnd = line;
+	}
+	
+	//temp? merge the two classes together?
+	public DataClipboardFunctions getFunctions(){
+		return functions;
+	}
 }
--- src/smp/clipboard/RubberBandEventHandler.java
+++ src/smp/clipboard/RubberBandEventHandler.java
@@ -17,7 +17,9 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
+import smp.components.Values;
 import smp.components.staff.Staff;
+import smp.components.staff.sequences.StaffNoteLine;
 import smp.stateMachine.StateMachine;
 
 /**
@@ -34,7 +36,7 @@ public class RubberBandEventHandler implements EventHandler<MouseEvent> {
     
     Staff theStaff;
 	StackPane rubberBandLayer;
-
+	DataClipboard theDataClipboard;
     /**
      * Margin at the edge of the scrollpane. When a rectangle is being created
      * and the mouse resizes into this margin, the scrollpane will scroll to
@@ -104,11 +106,47 @@ public class RubberBandEventHandler implements EventHandler<MouseEvent> {
 //        });
 //    }
 
-    public RubberBandEventHandler(Staff st, StackPane rbl) {
+    public RubberBandEventHandler(Staff st, StackPane rbl, DataClipboard clippy) {
 		// TODO Auto-generated constructor stub
     	theStaff = st;
     	rubberBandLayer = rbl;
     	rubberBands = new ArrayList<>();
+    	theDataClipboard = clippy;
+
+		// TEMPORARY, should probably be in a dataclipboardeventhandler
+		rbl.setFocusTraversable(true);
+    	rbl.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.isControlDown()) {
+					switch (event.getCode()) {
+					case C:
+						//select code should not be with the copy code obviously
+						System.out.println("COPY");
+						int lb = getLine(rubberBand.getTranslateX()) + StateMachine.getMeasureLineNum();
+						int pb = getPosition(rubberBand.getTranslateY());
+						int le = getLine(rubberBand.getTranslateX() + rubberBand.getWidth()) + StateMachine.getMeasureLineNum();
+						int pe = getPosition(rubberBand.getTranslateY() + rubberBand.getHeight());
+						theDataClipboard.getFunctions().select(lb, pb, le, pe);
+						theDataClipboard.getFunctions().copy();
+						for ( StaffNoteLine line : theDataClipboard.getData() ) {
+							if(!line.isEmpty())
+								System.out.println(line);
+						}
+						break;
+					case V:
+						System.out.println("PASTE");
+						int currentLine = getLine(mouseX) + StateMachine.getMeasureLineNum();
+						System.out.println(currentLine);
+						theDataClipboard.getFunctions().paste(currentLine);
+						break;
+					default:
+						break;
+					}
+				}
+			}
+		});
 	}
 
 	@Override
@@ -125,28 +163,24 @@ public class RubberBandEventHandler implements EventHandler<MouseEvent> {
 //                scrollPane.unhighlightAllVols();
 //                DataClipboard.clearSelection();
 //                
-            	rubberBandLayer.getChildren().removeAll(rubberBands);
-                rubberBands.clear();
-                rubberBand = null;
             } else {
-                rubberBand = new RubberBand();
-                rubberBands.add(rubberBand);
-                rubberBandLayer.getChildren().add(rubberBand);
             }
-//            
-            if (rubberBands.isEmpty()) {
-                rubberBand = new RubberBand();
-                rubberBands.add(rubberBand);
-                rubberBandLayer.getChildren().add(rubberBand);
-            }
-//            
+
+        	rubberBandLayer.getChildren().remove(rubberBand);
+            rubberBand = new RubberBand();
+        	rubberBandLayer.getChildren().add(rubberBand);
+            
             rubberBand.begin(mouseEvent.getX(), mouseEvent.getY());
+            System.out.print("begin GetLine:" + getLine(mouseEvent.getX()));
+            System.out.println(" GetPos:" + getPosition(mouseEvent.getY()));
 //            
         } else if (mouseEvent.isPrimaryButtonDown()) {
             rubberBand.resize(mouseEvent.getX(), mouseEvent.getY());
 //            navigatePane(mouseEvent);
         } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_RELEASED) {
             rubberBand.end();
+            System.out.print("end GetLine:" + getLine(mouseEvent.getX()));
+            System.out.println(" GetPos:" + getPosition(mouseEvent.getY()));
 //            
 //            Selection type = ((RibbonMenuMPO)Variables.stageInFocus.getRibbonMenu()).getSelectionToolbar().getSelectionType();
 //            if (type.equals(Selection.SELECTION_NOTES) || type.equals(Selection.SELECTION_NOTES_AND_VOL)) {
@@ -198,21 +232,93 @@ public class RubberBandEventHandler implements EventHandler<MouseEvent> {
 //        }
 //    }
 //    
-//    /**
-//     *
-//     * @param x mouse pos for entire scene
-//     * @param y mouse pos for entire scene
-//     * @return line based on x and y coord
-//     */
-//    private int getLine(double x, double y) {
+    /**
+	 *
+	 * 
+	 * 
+	 * @relevant Values.NOTELINES_IN_THE_WINDOW, StateMachine.getMeasureLineNum()
+	 * 
+	 * @param x
+	 *            mouse pos for entire scene
+	 * @return line based on x coord
+	 */
+	private int getLine(double x) {
+		// HARD CODED FOR NOW
+		if (x < 128 || x > 784) {
+			return -1;
+		}
+		return (((int) x - 128) / 64);
+	}
+	
+    /**
+	 *
+	 * temporary call this in RubberBand instead
+	 * 
+	 * @relevant Values.NOTES_IN_A_LINE
+	 * 
+	 * @param y
+	 *            mouse pos for entire scene
+	 * @return note based on y coord
+	 */
+	private int getPosition(double y) {
+		// HARD CODED FOR NOW
+		if (y < 0 || y > 304) {
+			return -1;
+		}
+		return (((int) y - 0) / 16);
+	}
+//  public int getLineBegin() {
+//  //x, 32px is exactly at the line
+//  /* lineOffsetX is indices 0-31 of the row */
+//  int lineOffsetX = 0;
+//  if (this.getTranslateX() < 122 + 32) {//122 is arbitrary 
+//      lineOffsetX = 0;
+//  } else if (this.getTranslateX() > Constants.WIDTH_DEFAULT - 48 + 32 - 64) {//48 is arbitrary
+////      return -1;
+//      lineOffsetX = 32;
+//  } else {
+//      lineOffsetX = ((int)this.getTranslateX() - (122 + 32)) / Constants.LINE_SPACING + 1;
+//  }
 //
-//        if (x < 122 || x > Constants.WIDTH_DEFAULT - 48 || !zby(y))//122 is arbitrary, 48 is arbitrary
-//        {
-//            return -1;
-//        }
-//        return (((int) x - 122) / 64)
-//                + ((int) y / Constants.ROW_HEIGHT_TOTAL) * Constants.LINES_IN_A_ROW;
-//    }
+//  //y
+//  /* lineOffsetY is row */
+//  int lineOffsetY = 0;
+//  if(!zby(this.getTranslateY())){
+//      lineOffsetY = (int)this.getTranslateY() / Constants.ROW_HEIGHT_TOTAL + 1;
+//  } else {
+//      lineOffsetY = (int)this.getTranslateY() / Constants.ROW_HEIGHT_TOTAL;
+//  }
+//  System.out.println("BEG" + lineOffsetY * Constants.LINES_IN_A_ROW + " " + lineOffsetX);
+//	return lineOffsetY * Constants.LINES_IN_A_ROW + lineOffsetX;
+////  return getLine(xOrigin, yOrigin);
+//}
+//
+//public int getLineEnd() {
+//  //x, 32px is exactly at the line
+//  /* lineOffsetX is indices 0-31 of the row */
+//  int lineOffsetX = 0;
+//  if ((this.getTranslateX() + this.getWidth()) < 122 + 32) {//122 is arbitrary 
+////      return -1;
+//      lineOffsetX = -1;
+//  } else if ((this.getTranslateX() + this.getWidth()) > Constants.WIDTH_DEFAULT - 48 + 32) {//48 is arbitrary
+//      lineOffsetX = 31;
+//  } else {
+//      lineOffsetX = ((int)(this.getTranslateX() + this.getWidth()) - (122 + 32 + 64)) / Constants.LINE_SPACING + 1;
+//  }
+//
+//  //y
+//  /* lineOffsetY is row */
+//  int lineOffsetY = 0;
+//  if(!zby(this.getTranslateY() + this.getHeight())){
+//      lineOffsetY = (int)(this.getTranslateY() + this.getHeight()) / Constants.ROW_HEIGHT_TOTAL;
+//  } else {
+//      lineOffsetY = (int)(this.getTranslateY() + this.getHeight()) / Constants.ROW_HEIGHT_TOTAL;
+//  }
+//  System.out.println("END" + lineOffsetY * Constants.LINES_IN_A_ROW + " " + lineOffsetX);
+//	return lineOffsetY * Constants.LINES_IN_A_ROW + lineOffsetX;
+////  return getLine(xOrigin + this.getWidth(), yOrigin + this.getHeight());
+//}	
+	
 //
 //    /* If valid y */
 //    private boolean zby(double y) {
@@ -261,4 +367,8 @@ public class RubberBandEventHandler implements EventHandler<MouseEvent> {
 //            }
 //        }
 //    }
+	
+	public RubberBand getRubberBand() {
+		return rubberBand;
+	}
 }
