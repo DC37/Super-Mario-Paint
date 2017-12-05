600e3807e05983491674b3b900df869c7af06bc7
j574y923 <j574y923@gmail.com>
2017-12-05 17:43:07 -0600

    comment out print statements

--- src/smp/components/staff/StaffInstrumentEventHandler.java
+++ src/smp/components/staff/StaffInstrumentEventHandler.java
@@ -208,7 +208,7 @@ public class StaffInstrumentEventHandler implements EventHandler<Event> {
                 line);
         sveh.updateVolume();
         theStaff.redraw();
-        System.out.println(theStaffNote + " " + theStaffNote.getPosition());
+//        System.out.println(theStaffNote + " " + theStaffNote.getPosition());
     }
 
     /**
