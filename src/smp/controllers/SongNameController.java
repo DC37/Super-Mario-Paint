728abf9cf5a30d26765ac39e29cc605e6e22042c
j574y923 <j574y923@ku.edu>
2018-09-30 18:36:22 -0500

    add songnamecontroller and bidirectional binding in setupviewupdater

---
+++ src/smp/controllers/SongNameController.java
@@ -0,0 +1,87 @@
+package smp.controllers;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import smp.fx.SMPFXController;
+import smp.models.Variables;
+
+/**
+ * Use this to add several eventhandlers and a focus listener to the song name
+ * <code>TextField</code> that will programmatically refocus the
+ * <code>ScrollBar</code> after clicking any element outside the textfield.
+ * 
+ * TODO: if we move to a MVC design pattern, move all textfield interaction into
+ * here.
+ * 
+ * @author J
+ * @since v1.1.2
+ *
+ */
+public class SongNameController {
+
+	TextField songName;
+	SMPFXController controller;
+
+	/**
+	 * The event handler that will handle unfocusing the TextField and
+	 * refocusing the ScrollBar. 
+	 */
+	EventHandler<MouseEvent> unfocusMouseEventHandler;
+
+	boolean mouseExited = false;
+
+	public SongNameController(TextField tf) {
+		this.songName = tf;
+
+		// click somewhere to unfocus only if the mouse exited
+		unfocusMouseEventHandler = new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				if (mouseExited)
+					controller.getScrollbar().requestFocus();
+			}
+		};
+
+		// when we get focused, prepare for the unfocusing mouse press event
+		// when we get unfocused, clean up the event filter
+		songName.focusedProperty().addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue,
+					Boolean newPropertyValue) {
+				if (newPropertyValue) {
+					songName.getScene().addEventFilter(MouseEvent.MOUSE_PRESSED, unfocusMouseEventHandler);
+				} else {
+					songName.getScene().removeEventFilter(MouseEvent.MOUSE_PRESSED, unfocusMouseEventHandler);
+				}
+			}
+		});
+
+		// mouse enters thus mouse has not exited, we cannot unfocus
+		songName.addEventFilter(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				mouseExited = false;
+			}
+		});
+		
+		// mouse exited, we can unfocus
+		songName.addEventFilter(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				mouseExited = true;
+			}
+		});
+		
+		setupViewUpdater();
+	}
+	
+    private void setupViewUpdater() {
+    	songName.textProperty().bindBidirectional(Variables.songName);
+    }
+}
--- src/smp/models/Variables.java
+++ src/smp/models/Variables.java
@@ -2,6 +2,7 @@ package smp.models;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.StringProperty;
 import smp.components.InstrumentIndex;
 import smp.components.Values;
 
@@ -10,11 +11,13 @@ public class Variables {
 	public static ObjectProperty<StaffArrangement> loadedArrangement;
 	public static ObjectProperty<InstrumentIndex> selectedInstrument;
 	public static BooleanProperty[] noteExtensions;
+	public static StringProperty songName;
 	
 	public Variables() {
 		loadedSequence.set(new StaffSequence());
 		loadedArrangement.set(new StaffArrangement());
 		selectedInstrument.set(InstrumentIndex.MARIO);
 		noteExtensions = new BooleanProperty[Values.NUMINSTRUMENTS];
+		songName.set("");
 	}
 }
