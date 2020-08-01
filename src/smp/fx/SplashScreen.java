28a4d41bf744406c7a03c4efa78b08f83170c916
SeymourSchlong <36908123+SeymourSchlong@users.noreply.github.com>
2020-07-31 20:11:42 -0400

    Restrict loading animation dimensions
    Restricts dimensions to 236x36 to allow for upscaling

--- src/smp/fx/SplashScreen.java
+++ src/smp/fx/SplashScreen.java
@@ -46,6 +46,8 @@ public class SplashScreen extends Preloader {
         // @since 1.4, to spice up the load screen. why not? - seymour 
         imageview = new ImageView();
         imageview.setImage(new Image("file:./sprites/LOADING_ANIM.gif"));
+        imageview.setFitWidth(236);
+		imageview.setFitHeight(36);
         imageview.setTranslateX(32);
         imageview.setTranslateY(32);
         p.setTop(imageview);
