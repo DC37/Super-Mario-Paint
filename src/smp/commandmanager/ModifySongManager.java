fcef89b1c18d5359208d69c774f67570fb85b596
rozlynd <rozlyn.df@gmail.com>
2025-03-09 13:20:44 +0100

    Setup CM event handler in controller

--- src/smp/commandmanager/ModifySongManager.java
+++ src/smp/commandmanager/ModifySongManager.java
@@ -1,36 +1,12 @@
 package smp.commandmanager;
 
-import javafx.event.EventHandler;
-import javafx.scene.input.KeyEvent;
-import smp.fx.SMPFXController;
-
 public class ModifySongManager extends CommandManager {
 
 	private Runnable refreshDisplay;
-	private SMPFXController controller;
 	
-	public ModifySongManager(Runnable refreshDisplay, SMPFXController ct) {
+	public ModifySongManager(Runnable refreshDisplay) {
 		super();
 		this.refreshDisplay = refreshDisplay;
-		controller = ct;
-		
-		controller.getBasePane().addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>(){
-
-			@Override
-			public void handle(KeyEvent event) {
-				if(event.isControlDown())
-					switch(event.getCode()) {
-					case Y:
-						redo();
-						break;
-					case Z:
-						undo();
-						break;
-					default:
-						break;
-					}
-			}
-		});
 	}
 	
 	public void redo() {
