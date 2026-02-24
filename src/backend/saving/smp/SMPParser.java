e147ea2d7d4c028badacfd8f5b7135adafd26a9b
rozlynd <rozlyn.df@gmail.com>
2026-02-24 16:58:04 +0100

    Separate Song and Arrangement parsers

--- src/backend/saving/smp/SMPParser.java
+++ src/backend/saving/smp/SMPParser.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import backend.saving.mpc.MPCDecoder;
-import backend.songs.StaffArrangement;
 import backend.songs.StaffNote;
 import backend.songs.StaffNoteLine;
 import backend.songs.StaffSequence;
@@ -30,12 +29,12 @@ public class SMPParser {
 	 * @throws IOException
 	 * @throws ClassNotFoundException
 	 */
-	public static StaffSequence parseSong(File inputFile)
+	public static StaffSequence parse(File inputFile)
 			throws FileNotFoundException, IOException, NullPointerException {
 		FileInputStream f_in = new FileInputStream(inputFile);
 		StaffSequence loaded = null;
 		try {
-			loaded = MPCDecoder.parseSong(inputFile);
+			loaded = MPCDecoder.parse(inputFile);
 		} catch (ParseException e1) {
 			try {
 				// Decode as object
@@ -123,64 +122,6 @@ public class SMPParser {
 			}
 		}
 		loaded.normalize();
-		return loaded;
-	}
-
-	/**
-	 * Loads an arrangement from the file specified.
-	 *
-	 * @param inputFile
-	 *            The file to load from.
-	 * @return A loaded arrangement file. The format is StaffArrangement.
-	 * @throws FileNotFoundException
-	 * @throws IOException
-	 * @throws ClassNotFoundException
-	 */
-	public static StaffArrangement parseArrangement(File inputFile)
-			throws FileNotFoundException, IOException, ClassNotFoundException {
-		FileInputStream f_in = new FileInputStream(inputFile);
-		StaffArrangement loaded = null;
-		try {
-			ObjectInputStream o_in = new ObjectInputStream(f_in);
-			loaded = (StaffArrangement) o_in.readObject();
-			o_in.close();
-		} catch (ClassNotFoundException | ClassCastException | EOFException
-				| StreamCorruptedException e) {
-			// If it's not an object, try using the human-readable option.
-			f_in.close();
-			f_in = new FileInputStream(inputFile);
-			Scanner sc = new Scanner(f_in);
-			ArrayList<String> read = new ArrayList<String>();
-			while (sc.hasNext()) {
-				read.add(sc.nextLine());
-			}
-			sc.close();
-			loaded = parseArrText(read);
-		}
-		f_in.close();
-		return loaded;
-	}
-
-	/**
-	 * Parses a bunch of text from a save file and makes a
-	 * <code>StaffArrangement</code> out of it.
-	 *
-	 * @param read
-	 *            <code>ArrayList</code> of filenames and paths.
-	 * @return Hopefully, a decoded <code>StaffArrangement</code>
-	 */
-	private static StaffArrangement parseArrText(ArrayList<String> read) {
-		StaffArrangement loaded = new StaffArrangement();
-		File f = null;
-		ArrayList<File> files = new ArrayList<File>();
-		ArrayList<String> names = new ArrayList<String>();
-		for (String s : read) {
-			names.add(s);
-			f = new File(s + ".txt");
-			files.add(f);
-		}
-		loaded.setTheSequenceNames(names);
-		loaded.setTheSequenceFiles(files);
 		return loaded;
 	}
 
