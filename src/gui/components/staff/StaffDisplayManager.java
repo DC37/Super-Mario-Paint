51314efa50d45ab5cb8d39ee566c1793bacd98b5
rozlynd <rozlyn.df@gmail.com>
2026-04-04 15:17:44 +0200

    First measure subdivision indexed by 1

--- src/gui/components/staff/StaffDisplayManager.java
+++ src/gui/components/staff/StaffDisplayManager.java
@@ -413,7 +413,7 @@ public class StaffDisplayManager {
                 
             } else if (k > 0) {
                 currImage.setImage(imagesHolder.get(ImageIndex.STAFF_SLINE));
-                String txt = String.valueOf(currentBarIndex + 1) + "." + String.valueOf(k);
+                String txt = String.valueOf(currentBarIndex + 1) + "." + String.valueOf(k + 1);
                 double fontSize = Font.getDefault().getSize() * .75;
                 currText.setText(txt);
                 currText.setFont(new Font(fontSize));
