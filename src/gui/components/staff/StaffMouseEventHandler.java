88f04a96b9268616bf9def8806657dd80a936edd
Aura Lesse Programmer <aurasys.7608@zohomail.com>
2026-06-21 13:00:18 -0700

    refactor!: simplify double ternaries

--- src/gui/components/staff/StaffMouseEventHandler.java
+++ src/gui/components/staff/StaffMouseEventHandler.java
@@ -190,7 +190,11 @@ public class StaffMouseEventHandler implements EventHandler<MouseEvent> {
         if ((Settings.debug & 0b10000) != 0)
             System.out.println("Index: " + theInd + "\nPosition: "+ position + "\nAcc: " + acc + "\nVel: " + vel);
         
-        MuteModifier mod = muteA ? MuteModifier.MUTE_THIS_INST : mute ? MuteModifier.MUTE_THIS_PITCH : MuteModifier.REGULAR;
+        MuteModifier mod;
+        if (muteA)     mod = MuteModifier.MUTE_THIS_INST;
+        else if (mute) mod = MuteModifier.MUTE_THIS_PITCH;
+        else           mod = MuteModifier.REGULAR;
+        
         StaffNote theStaffNote = new StaffNote(theInd, position, acc, mod);
         
         if (!mute && !muteA)
