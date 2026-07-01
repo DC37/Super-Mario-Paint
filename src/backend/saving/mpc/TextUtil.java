deeddc3bfe8f60d4fa68b6b5cfdaf39b3e5c05fb
Aura Lesse Programmer <aurasys.7608@zohomail.com>
2026-07-01 15:02:51 -0700

    fix: deactivate linter rule instace for explicit digit regex

--- src/backend/saving/mpc/TextUtil.java
+++ src/backend/saving/mpc/TextUtil.java
@@ -25,7 +25,8 @@ public class TextUtil {
      * This is the regex pattern that denotes a note in an MPC text file. It
      * should also be able to catch "glitch" notes of the form a17
      */
-    private static final String NOTE = "(([a-z][a-z0-9]\\d?[#;]?)?)";
+	@SuppressWarnings("java:S6353")
+    private static final String NOTE = "(([a-z][a-z0-9][0-9]?[#;]?)?)";
 
     /** This is the regex pattern that denotes a kind of a concat operator. */
     private static final String PLUS = "\\+";
