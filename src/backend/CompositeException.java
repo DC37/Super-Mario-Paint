6013056b9332797cf967223c4149c983bc135b73
Aura Lesse Programmer <aurasys.7608@zohomail.com>
2026-08-10 08:30:21 -0700

    chore(clean): favor repeat impl from apache commons

--- src/backend/CompositeException.java
+++ src/backend/CompositeException.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
-import utilities.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class CompositeException extends Exception {
 
